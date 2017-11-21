2d00329bdb837d431c0d54cd23b3a6aed79ba032
liu <903495291@qq.com>
2017-11-21 13:44:04 +0800

    my project

---
+++ lkservice/src/main/java/com/lk/service/TeacherService.java
@@ -0,0 +1,14 @@
+package com.lk.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+
+
+@WebService
+public interface TeacherService {
+    @WebMethod
+    public String selTeacher(int id);
+}
